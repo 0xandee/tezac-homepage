3db768dc9fed059758eee756b503945cee5b69c1
anvospace <34095856+0xandee@users.noreply.github.com>
2025-02-13 23:08:56 +0700

    add logos

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import React from "react";
 import { socials } from "@/lib/contants";
-
+import Image from "next/image"
 
 const Footer = () => {
   return (
     <div className="bg-zinc-800 px-0 md:px-20">
       <div className="bg-white rounded-tl-none md:rounded-tl-[4rem] rounded-tr-none md:rounded-tr-[4rem] py-5 md:py-10">
         <div className=" px-0 md:px-20 flex flex-col md:flex-row items-center justify-center md:justify-between">
-          <h1 className="font-extrabold py-10 md:py-0 text-2xl md:text-4xl">TEZAC</h1>
+          <Image src="/tezac-text-black.svg" alt="tezac logo" width={156} height={40} />
           <div id="socials" className="flex items-center gap-7 md:gap-5">
             {socials.map((social, index) => (
               <motion.div
--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,9 @@ import { Button } from "./ui/button"
 const Header = () => {
   return (
     <div className="flex items-center justify-between py-6 bg-zinc-800 px-5 md:px-20">
-      <div className="flex align-middle gap-2"><Image src="/tezac.png" alt="tezac logo" width={30} height={20} /><span className="text-xl font-bold tracking-wider text-white ">TEZAC</span></div>
+      <div className="flex align-middle gap-2">
+        <Image src="/tezac-logo-horizontal.svg" alt="tezac logo" width={156} height={40} />
+      </div>
       <Button className="text-sm bg-transparent text-white hover:bg-transparent shadow-none whitespace-nowrap sm:block border-none font-light">Coming s00n</Button>
     </div>
   )
