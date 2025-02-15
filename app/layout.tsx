6ef93667e175f101077b9fb79bdbf63b184a0b7c
anvospace <34095856+0xandee@users.noreply.github.com>
2025-02-15 18:20:22 +0700

    Add Vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const geistMono = Geist_Mono({
@@ -102,6 +103,7 @@ export default function RootLayout({
       </head>
       <body className={geistMono.className}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
