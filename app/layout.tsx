db92337d530426e6fc6e05f69d5c78b7805da72a
anvospace <34095856+0xandee@users.noreply.github.com>
2025-02-15 18:31:39 +0700

    Add Vercel Analytics Nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const geistMono = Geist_Mono({
