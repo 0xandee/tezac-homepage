70fa48529aebd519d6dd1ede2ea1cbd165c93aa9
0xandee <34095856+0xandee@users.noreply.github.com>
2025-03-16 23:21:04 +0700

    Refactor Hero component by removing unused state and commented code; update NeonGradientCard prop type from 'any' to 'unknown'.

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,9 +2,8 @@
 import { motion } from "framer-motion"
 import { TextRevealButton } from "./animated-components/text-reveal-button"
 import Description from "./description"
-import { EncryptedText, generateRandomSymbols } from "./animated-components/encrypted-text-effect"
-import { useState } from "react"
 import { MorphingText } from "./magicui/morphing-text"
+import { generateRandomSymbols } from "./animated-components/encrypted-text-effect"
 
 const titleTexts = [
   "Privately",
@@ -19,7 +18,6 @@ const subtitleTexts = [
 ];
 
 export default function Hero() {
-  const [isHovered, setIsHovered] = useState(false);
   return (
     <div className="relative min-h-screen w-full bg-transparent">
       <motion.div
@@ -31,35 +29,6 @@ export default function Hero() {
         <div className="flex min-h-[80vh] flex-col items-center justify-center text-center">
           <MorphingText className="text-primary-green" texts={titleTexts} />
           <MorphingText className="text-white" texts={subtitleTexts} />
-          {/* <motion.h1
-            className="max-w-4xl font-semibold leading-none text-[2.8rem] md:text-8xl whitespace-normal sm:whitespace-nowrap text-zinc-500"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0, duration: 0.2 }}
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-          >
-            <EncryptedText
-              key={isHovered ? "revealed" : "encrypted"}
-              text={isHovered ? generateRandomSymbols("Privately".length) : "Privately"}
-              interval={25}
-            />
-          </motion.h1>
-          <motion.h1
-            className="max-w-4xl font-semibold text-[2.8rem] md:text-8xl whitespace-normal sm:whitespace-nowrap leading-[64px] mt-2 sm:mt-4 text-white"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0, duration: 0.2 }}
-
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-          >
-            <EncryptedText
-              key={isHovered ? "revealed" : "encrypted"}
-              text={isHovered ? generateRandomSymbols("Own✕Trade NFTs".length) : "Own✕Trade NFTs"}
-              interval={25}
-            />
-          </motion.h1> */}
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
