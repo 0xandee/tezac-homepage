4780064539a2cea4ac63ace4f14d5ad14300891e
0xandee <34095856+0xandee@users.noreply.github.com>
2025-03-19 22:11:15 +0700

    Refactor Footer component to improve layout by adjusting padding and responsiveness, ensuring better alignment and spacing for content.

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,9 +11,9 @@ import { cloneElement } from 'react';
 const Footer = () => {
   const [inView, setInView] = useState(false);
   return (
-    <div className="px-0 md:px-20">
-      <div className="bg-white rounded-tl-[2rem] md:rounded-tl-[4rem] rounded-tr-[2rem] md:rounded-tr-[4rem] py-10 md:py-10">
-        <div className="px-10 md:px-20 flex flex-row items-center justify-between gap-10">
+    <div className="px-0">
+      <div className="bg-white py-10 md:py-10 px-8 md:px-12 lg:px-24 xl:px-40 2xl:px-64">
+        <div className="flex flex-row items-center justify-between gap-10">
           <Image src="/tezac-text-black.svg" alt="tezac logo" width={156} height={40} className="w-24 md:w-[128px]" />
           <div id="socials" className="flex items-center gap-6">
             {socials.map((social, index) => (
